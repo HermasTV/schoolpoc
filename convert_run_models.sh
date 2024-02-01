a9e10107490640c2fdd7d8035ce7e1d2bfaa5f23
Hermas <montada7@gmail.com>
2024-02-01 10:56:32 +0200

    fix dockerfile

---
+++ convert_run_models.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+#
+echo "Converting ONNX models to TensorRT .plan models if not already converted ... "
+
+
+
+# Before conversion make sure that the plan files are not present in the model repository 
+# convert Face Detection ONNX model to tensorrt .plan model
+
+# --explicitBatch --minShapes=input.1:1,3,640,640 --optShapes=input.1:1,3,640,640 --maxShapes=input.1:1,3,640,640
+#  --explicitBatch --minShapes=data:1,3,112,112 --optShapes=data:1,3,112,112 --maxShapes=data:1,3,112,112
+
+if [ ! -f /models/SCRTRT_2/0/model.plan ]; then
+    echo "Face Detection model not found, converting it to .plan"
+    /usr/src/tensorrt/bin/trtexec --onnx=/onnx-models/scrfd_2.5.onnx --saveEngine=/models/SCRTRT_2/0/model.plan 
+fi
+# convert Face Recognition ONNX model to tensorrt .plan model
+if [ ! -f /models/arcface/0/model.plan ]; then
+    echo "Face Recognition model not found, converting it to .plan"
+    /usr/src/tensorrt/bin/trtexec --onnx=/onnx-models/model.onnx --saveEngine=/models/arcface/0/model.plan 
+fi
+# run the triton server
+echo "Starting the Triton Inference Server ..."
+tritonserver --model-repository=/models;
--- dockerfile
+++ dockerfile
@@ -7,7 +7,18 @@ WORKDIR /app
 COPY model_repository /models
 
 # Copy the SCRFD/models directory into the container
-COPY SCRFD/models /onnx-models
+COPY onnx-models /onnx-models
 
-# Run the tritonserver command when the container launches
-CMD ["tritonserver", "--model-repository=/models"]
+# copy the models converter script into the container
+COPY convert_run_models.sh /app/convert_run_models.sh
+
+# run the models converter script
+RUN chmod 777 /app/convert_run_models.sh
+
+# Run the convert_run_models.sh script in terminal when the container launches
+CMD ["/bin/bash", "-c", "/app/convert_run_models.sh"]
+
+# to build the docker image run the following command
+# docker build -t tritonserver:23.09-py3 .
+# to run the docker image run the following command
+# docker run --gpus all -it --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p8000:8000 -p8001:8001 -p8002:8002 tritonserver:23.09-py3
